834d02f17ccf87e9830aff6ab181aa979a1621ae
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-03-22 18:36:32 +0100

    feat: Setup initial packages for wishes API

---
+++ packages/local_storage_wishes_api/lib/src/local_storage_wishes_api.dart
@@ -0,0 +1,7 @@
+/// {@template local_storage_wishes_api}
+/// A Flutter implementation of the WishesApi that uses local storage.
+/// {@endtemplate}
+class LocalStorageWishesApi {
+  /// {@macro local_storage_wishes_api}
+  const LocalStorageWishesApi();
+}
---
+++ packages/wishes_repository/test/src/wishes_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:wishes_repository/wishes_repository.dart';
+
+void main() {
+  group('WishesRepository', () {
+    test('can be instantiated', () {
+      expect(WishesRepository(), isNotNull);
+    });
+  });
+}
